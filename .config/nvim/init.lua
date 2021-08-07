e79bd1f4097af4ac4b97e68a62ed8565ae4c14d4
Jakob Hansen <jakob.hansen@hotmail.no>
2021-08-07 15:50:03 +0200

    Add init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,88 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '$HOME/.config/nvim/plugged')
+
+    --"Plugin dependencies"
+	Plug 'nvim-lua/popup.nvim'
+	Plug 'nvim-lua/plenary.nvim'
+	Plug 'RishabhRD/popfix'
+	Plug 'rbgrouleff/bclose.vim'
+
+	--"Text manipulation"
+	Plug 'scrooloose/nerdcommenter'
+	Plug 'windwp/nvim-autopairs'
+
+	--"Movement"
+	Plug 'psliwka/vim-smoothie'
+
+	--"Syntax highlighting"
+	Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn['TSUpdate']})
+	Plug 'sheerun/vim-polyglot'
+
+	--"Buffers"
+	Plug 'troydm/zoomwintab.vim'
+
+	--"Themes and Visuals"
+	Plug 'christianchiarulli/nvcode-color-schemes.vim'
+	Plug 'vim-airline/vim-airline'
+	Plug 'vim-airline/vim-airline-themes'
+	Plug 'kyazdani42/nvim-web-devicons'
+	Plug 'folke/tokyonight.nvim'
+
+
+	--"Files and git"
+	Plug '907th/vim-auto-save'
+	Plug 'kyazdani42/nvim-tree.lua'
+	Plug 'tpope/vim-fugitive'
+	Plug 'idanarye/vim-merginal'
+	Plug 'rhysd/conflict-marker.vim'
+
+	--"Menus"
+	Plug 'nvim-telescope/telescope.nvim'
+	Plug 'mhinz/vim-startify'
+	Plug 'spinks/vim-leader-guide'
+
+	--"LSP"
+	Plug 'neovim/nvim-lspconfig'
+	Plug 'RishabhRD/nvim-lsputils'
+	Plug 'hrsh7th/nvim-compe'
+	Plug 'mfussenegger/nvim-jdtls'
+	Plug 'jose-elias-alvarez/nvim-lsp-ts-utils'
+
+	--"Snippets"
+	Plug 'L3MON4D3/LuaSnip'
+
+	--"Notes"
+	Plug 'reedes/vim-pencil'
+	Plug 'instant-markdown/vim-instant-markdown'
+	Plug 'lervag/vimtex'
+	Plug 'vhyrro/neorg'
+
+	--"Random"
+	Plug 'andweeb/presence.nvim'
+    Plug 'svermeulen/vimpeccable'
+
+vim.call('plug#end')
+
+-- Require modules
+
+require('opts')
+require('maps')
+require('leadermaps')
+
+-- Require plugins
+require('plugins.treesitter-config')
+require('plugins.nvim-compe-config')
+require('plugins.lsp-config')
+require('plugins.lsp-servers')
+require('plugins.lsputils-config')
+require('plugins.luasnips')
+require('plugins.neorg-config')
+require('plugins.nvim-tree-config')
+require('plugins.telescope-config')
+require('plugins.auto-save-config')
+require('plugins.airline-config')
+require('plugins.startify-config')
+require('plugins.nerdcommenter-config')
+
+require('plugins.nvim-autopairs-config')
