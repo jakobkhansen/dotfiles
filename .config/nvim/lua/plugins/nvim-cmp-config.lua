716a700d6d44e9183978ecbafa278d09595eda72
Jakob Hansen <jakob.hansen@hotmail.no>
2021-10-19 13:55:19 +0200

    Updates

--- .config/nvim/lua/plugins/nvim-cmp-config.lua
+++ .config/nvim/lua/plugins/nvim-cmp-config.lua
@@ -8,7 +8,7 @@ cmp.setup {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<CR>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
+            --select = true,
         })
     },
 
