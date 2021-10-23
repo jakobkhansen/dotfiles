6d1cdfcf7eaa45a7c1b2027806b7f02134b7c9c8
Jakob Hansen <jakob.hansen@hotmail.no>
2021-10-23 02:14:45 +0200

    Updates

--- .config/nvim/lua/language_configs.lua
+++ .config/nvim/lua/language_configs.lua
@@ -1,4 +1,4 @@
 local vimscript = vim.api.nvim_exec
 
-vimscript('au FileType javascript,typescript,typescriptreact setlocal sts=2 ts=2 sw=2', false)
+vimscript('au FileType javascript,javascriptreact,typescript,typescriptreact setlocal sts=2 ts=2 sw=2', false)
 vimscript('au FileType norg setlocal sts=2 ts=2 sw=2', false)
