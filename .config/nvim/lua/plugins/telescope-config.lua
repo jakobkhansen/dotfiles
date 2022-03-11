8c9f2e5782acfaae2644a1a4c8a4ea85570da630
Jakob Hansen <jakob.hansen@hotmail.no>
2022-03-11 14:27:57 +0100

    Updates

--- .config/nvim/lua/plugins/telescope-config.lua
+++ .config/nvim/lua/plugins/telescope-config.lua
@@ -12,12 +12,17 @@ local state = require("telescope.state")
 local builtin = require("telescope.builtin")
 local fb_finders = require("telescope._extensions.file_browser.finders")
 
+local lastDirectory
+
 local get_target_dir = function(finder)
 	local entry_path
-	if finder.files == false then
+	if finder.files == nil then
+		return lastDirectory or vim.fn.getcwd()
+	elseif finder.files == false then
 		local entry = action_state.get_selected_entry()
 		entry_path = entry and entry.value -- absolute path
 	end
+	lastDirectory = finder.files and finder.path or entry_path
 	return finder.files and finder.path or entry_path
 end
 
@@ -28,6 +33,31 @@ local open_in = function(finder, opts)
 		local dir = get_target_dir(curr_finder)
 		opts.cwd = dir
 		finder(opts)
+	end
+end
+
+local open_file_browser = function(files)
+	return function(prompt_bufnr)
+		local current_picker = action_state.get_current_picker(prompt_bufnr)
+		local curr_finder = current_picker.finder
+		local dir = get_target_dir(curr_finder)
+		print(dir)
+		-- print('Telescope file_browser path=' .. dir .. 'files=' .. tostring(files))
+		vim.cmd(
+			"Telescope file_browser hide_parent_dir=true, cwd_to_path=true path=" .. dir .. " files=" .. tostring(files)
+		)
+	end
+end
+
+local goto_parent_dir_check = function()
+	return function(prompt_bufnr)
+		local current_picker = action_state.get_current_picker(prompt_bufnr)
+		if current_picker ~= nil then
+			local curr_finder = current_picker.finder
+			if curr_finder ~= nil and curr_finder.files ~= nil then
+				fb_actions.goto_parent_dir(prompt_bufnr)
+			end
+		end
 	end
 end
 
@@ -47,16 +77,18 @@ require("telescope").setup({
 
 					return true
 				end,
-				["<BS>"] = fb_actions.goto_parent_dir,
+				["<BS>"] = goto_parent_dir_check(),
 
 				["<A-f>"] = open_in(builtin.find_files, {}),
-				["<A-d>"] = open_in(fb_picker, { files = false, hide_parent_dir=true, cwd_to_path=true }),
-				["<A-b>"] = open_in(fb_picker, { hide_parent_dir=true, cwd_to_path=true }),
+				["<A-d>"] = open_file_browser(false),
+				["<A-b>"] = open_file_browser(true),
 			},
 			i = {
+				["<BS>"] = goto_parent_dir_check(),
 				["<A-f>"] = open_in(builtin.find_files, {}),
-				["<A-d>"] = open_in(fb_picker, { files = false, hide_parent_dir=true, cwd_to_path=true }),
-				["<A-b>"] = open_in(fb_picker, { hide_parent_dir=true, cwd_to_path=true }),
+				["<A-g>"] = open_in(builtin.git_files, {}),
+				["<A-d>"] = open_file_browser(false),
+				["<A-b>"] = open_file_browser(true),
 			},
 		},
 	},
