758ecdcddbe160385243dccadee81b0319ecb00b
Jakob Hansen <jakobhansen@microsoft.com>
2023-11-30 13:27:52 +0100

    Updates

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,10 @@ function GoHome {
     cd ~
 }
 
+function Ls {
+    ls
+}
+
 function Dfiles {
     git --git-dir=C:\Users\jakobhansen\.dotfiles --work-tree=C:\Users\jakobhansen $args
 }
@@ -9,4 +13,5 @@ function Dfiles {
 Set-Alias h GoHome -Option AllScope
 Set-Alias n nvim
 Set-Alias g git
+Set-Alias l Ls
 Set-Alias dotfiles Dfiles -Option AllScope
