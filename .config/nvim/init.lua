bcad5b29250c26d04b5d44d6e80be566737d86a4
Jakob Hansen <jakobhansen@microsoft.com>
2024-01-16 16:08:05 +0100

    Updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -56,7 +56,7 @@ require("lazy").setup({
 
     {
         "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
+        branch = "v3.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "kyazdani42/nvim-web-devicons",
