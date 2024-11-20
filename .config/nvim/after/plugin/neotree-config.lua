040fbf3b6a98dfab5622448bbc7ccb44c68802ae
Jakob Hansen <jakobhansen@microsoft.com>
2024-11-20 10:51:22 +0100

    Updates

--- .config/nvim/after/plugin/neotree-config.lua
+++ .config/nvim/after/plugin/neotree-config.lua
@@ -94,6 +94,9 @@ require("neo-tree").setup({
         },
     },
     default_component_configs = {
+        container = {
+            enable_character_fade = false
+        },
         icon = {
             folder_empty = "",
         },
