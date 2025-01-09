8d2339b931627ece4b320179337f3d169cf695b9
Jakob Hansen <jakobhansen@microsoft.com>
2025-01-09 10:16:53 +0100

    Updates

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -10,8 +10,15 @@ function Dfiles {
     git --git-dir=C:\Users\jakobhansen\.dotfiles --work-tree=C:\Users\jakobhansen $args
 }
 
+function FuzzDir {
+    cd $(fzf --walker=dir)
+}
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
 Set-Alias h GoHome -Option AllScope
 Set-Alias n nvim
 Set-Alias g git
 Set-Alias l Ls
+Set-Alias ff FuzzDir
 Set-Alias dotfiles Dfiles -Option AllScope
