4f1090b06ecaf844d6cbcc82d8f3e8264bff5308
Jakob Hansen <jakob.hansen@hotmail.no>
2025-11-28 13:13:59 +0100

    Updates

--- .config/nvim/after/plugin/treesitter-config.lua
+++ .config/nvim/after/plugin/treesitter-config.lua
@@ -2,7 +2,7 @@ local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
 
 require("nvim-treesitter.configs").setup({
     ensure_installed = "all",
-    ignore_install = { "systemverilog", "norg" },
+    ignore_install = { "systemverilog", "norg", "ipkg" },
     highlight = {
         enable = true,
         disable = { "latex" },
