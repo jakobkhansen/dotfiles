1dead9df4441a6fb4d761d908ac37aec36778ea5
Jakob Hansen (from Dev Box) <jakobhansen@microsoft.com>
2025-12-08 14:18:46 +0100

    Updates

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,17 @@
 Remove-Item Alias:r
+
+# For keeping CWD when duplicating tab
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+  $out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
+
 function GoHome {
     cd ~
 }
