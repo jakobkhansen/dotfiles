562ade2564aba784307798fa8346fef20cb360e1
Jakob Hansen <jakob.hansen@hotmail.no>
2025-12-16 22:39:09 +0100

    Updates

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,65 @@
+Import-Module PSReadline
+Remove-Item Alias:r
+
+# For keeping CWD when duplicating tab
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+  $out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# enable up/down arrows for navigating through the history
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# moves the cursor to the end of the autocompleted command (remove if you want the cursor to remain where the completion started from)
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+# enable zsh autocompletion like auto completion
+
+Set-PSReadlineOption -PredictionSource History
+
+function GoHome {
+    cd ~
+}
+
+function Ls {
+    ls
+}
+
+function Dfiles {
+    git --git-dir=C:\Users\jakobhansen\.dotfiles --work-tree=C:\Users\jakobhansen $args
+}
+
+function FuzzDir {
+    cd $(fzf --walker=dir)
+}
+
+function r {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+$env:YAZI_FILE_ONE = "C:\Program Files\Git\usr\bin\file.exe"
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Set-Alias h GoHome -Option AllScope
+Set-Alias n nvim
+Set-Alias g git
+Set-Alias l Ls
+Set-Alias ff FuzzDir
+Set-Alias dotfiles Dfiles -Option AllScope
